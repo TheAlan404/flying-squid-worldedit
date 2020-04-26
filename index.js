bc85d9a7a2ca40510d9dbf395edc532b3c438aab
Glitch (thealan404-flying-squid-worldedit) <none>
2020-04-26 13:18:18 +0000

    Updated

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var prefix = "[WorldEdit] "
 console.log(prefix + "Plugin enabled.");
+let thereBe = "light";
 
 module.exports.serv = (serv) => {
+  
 };
 
 module.exports.player = (player, serv) => {
+  let pos1;
+  let pos2;
+  
+  
 };
